7a7b9a0ce43ea320abc29613922da5c717e9b599
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-07-22 00:12:15 +0300

    corected path to file

--- App0714 (globals, et.)/index.php
+++ App0714 (globals, et.)/index.php
@@ -11,7 +11,7 @@
     <h1 for="kmi">KMI masės indekso skaičiuoklė:</h1>
     <div class="content">
         <p>Į lentelę įveskite duomenis</p>
-        <form action="/App0714/calculated.php" method="POST" >
+        <form action="/App0714 (globals, et.)/calculated.php" method="POST" >
             <label>Jūsų ūgis (metrais, pvz.: 1.68)*:</label><input type="text" name="height"><br>
             <label>Jūsų svoris (kilogramais, pvz.: 50)*:</label><input type="text" name="weight"><br>
             <input type="submit" name="submit" value="Skaičiuoti" style="cursor: pointer; margin-left: 150px; margin-top: 10px; color: #FFF; background-color: #137AB2; border: none; border-radius: 5px; padding-top: 5px; padding-bottom: 5px">
