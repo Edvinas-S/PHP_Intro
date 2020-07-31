823a6be2c15df1bb5ba115a79f3d36c3613a22f1
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-07-31 14:56:52 +0300

    some MySQL query's

--- App0730 (Group By, Having et.)/index.php
+++ App0730 (Group By, Having et.)/index.php
@@ -23,33 +23,33 @@
             (3, "Petras", "Per", 500),
             (4, "Juozas", "Ten", 100);
 
-        //MIN salary
+        --MIN salary
         SELECT min(salary) FROM test_aggr;
 
-        //MIN and MAX salary
+        --MIN and MAX salary
         SELECT max(salary), min(salary) FROM test_aggr;
 
-        //SUM of salary
+        --SUM of salary
         SELECT sum(salary) FROM test_aggr;
 
-        // sum(salary) / count(salary) == avg(salary)
+        -- sum(salary) / count(salary) == avg(salary)
         SELECT avg(salary) FROM test_aggr; 
 
-        //no same double value
+        --no same double value
         SELECT group_concat(name), salary FROM test_aggr 
         GROUP BY salary;
 
-        // išrinkti unikalias salary reikšmes
+        -- išrinkti unikalias salary reikšmes
         select salary from test_aggr group by salary;
 
-        // kokie darbuotojai, gauna kažkokį konkretų atlyginimą
+        -- kokie darbuotojai, gauna kažkokį konkretų atlyginimą
         SELECT group_concat(name), salary
         FROM test_aggr 
         GROUP BY salary 
         ORDER BY salary DESC;
 
-        // Sujungia visas reikšmes stulpelyje, kurios yra ne null, pagal group by sąlygą. Pvz: sujugiame 
-            //kiekvienai eilutei vardus ir pavardes (į fullname) ir tada grupuodami sujungiame tuos vardus ir pavardes pagal salary stulpelį.
+        -- Sujungia visas reikšmes stulpelyje, kurios yra ne null, pagal group by sąlygą. Pvz: sujugiame 
+            --kiekvienai eilutei vardus ir pavardes (į fullname) ir tada grupuodami sujungiame tuos vardus ir pavardes pagal salary stulpelį.
         SELECT GROUP_CONCAT(CONCAT_WS(" ", name, surname)) as fullname, salary FROM test_aggr GROUP BY salary;
     -->
     <?php
